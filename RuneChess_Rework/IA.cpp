3fef94f1d09ccc877439f80919cb2063a31ca671
Thibault MICHEL <tmichel.ra@gmail.com>
2024-01-26 09:30:49 +0100

    Begin IA

--- RuneChess_Rework/IA.cpp
+++ RuneChess_Rework/IA.cpp
@@ -1,1 +1,13 @@
 #include "IA.h"
+
+void IA::Play(sf::RenderWindow* window, Board* board)
+{
+}
+
+void IA::GetBoardToFEM()
+{
+}
+
+void IA::ChooseMove()
+{
+}
--- RuneChess_Rework/IA.h
+++ RuneChess_Rework/IA.h
@@ -1,5 +1,13 @@
 #pragma once
+#include <SFML/Graphics.hpp>
+#include "Board.h"
+
 class IA
 {
+public:
+	void Play(sf::RenderWindow* window, Board* board);
+private :
+	void GetBoardToFEM();
+	void ChooseMove();
 };
 
